94ff9d741e8dbddda4bdee61f7ae05d193f34c22
Steven Sherry <steven.r.sherry@gmail.com>
2018-04-24 16:56:42 -0500

    feat: Added manual JSON parsing in JsonUtils.java
    Created helper methods to pull JSON values from JSON keys.
    This was performed by parsing the string and removing
    unwanted characters that could be presented to the user
    through an instance of Sandwich. The UI was changed
    to have a parent ConstraintLayout with an ImageView
    and a child ScrollView. The ScrollView contained a
    ConstraintLayout, which was used to present the information
    in a clean manner.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +60,31 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        TextView alsoKnownTv = findViewById(R.id.also_known_tv);
+        TextView originTv = findViewById(R.id.origin_tv);
 
+        descriptionTv.setText(sandwich.getDescription());
+
+        String ingredients = convertStringListToString(sandwich.getIngredients());
+        ingredientsTv.setText(ingredients);
+
+        String alsoKnownAs = convertStringListToString(sandwich.getAlsoKnownAs());
+        alsoKnownTv.setText(alsoKnownAs);
+
+        originTv.setText(sandwich.getPlaceOfOrigin());
+    }
+
+    private String convertStringListToString(List<String> list) {
+        if (list == null) { return "N/A"; }
+        StringBuilder sb = new StringBuilder();
+        for (String string: list) {
+            String commaSeparated = string + ", ";
+            sb.append(commaSeparated);
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        return sb.toString().replaceAll("([^a-zA-Z_0-9, ])", "");
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,51 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final int KEY_OFFSET = 4;
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        String mainName = retrieveStringValueFromJson("mainName", json);
+        List<String> alsoKnownAs = retrieveListValueFromJson("alsoKnownAs", json);
+        String placeOfOrigin = retrieveStringValueFromJson("placeOfOrigin", json);
+        String description = retrieveStringValueFromJson("description", json);
+        String image = retrieveStringValueFromJson("image", json);
+        List<String> ingredients = retrieveListValueFromJson("ingredients", json);
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
+
+    private static String retrieveStringValueFromJson(String key, String json) {
+        int keyEndIndex = json.indexOf("\"" + key + "\"" + ":\"") + key.length() + KEY_OFFSET;
+        int valueEndIndex = json.indexOf("\",", keyEndIndex);
+
+        String escape = "\\\\";
+        String rawString = json.substring(keyEndIndex, valueEndIndex);
+
+        if (rawString.isEmpty()) {
+            return "N/A"; 
+        }
+
+        return rawString.replaceAll(escape, "");
+    }
+
+    private static List<String> retrieveListValueFromJson(String key, String json) {
+        int keyEndIndex = json.indexOf("\"" + key + "\"") + key.length() + KEY_OFFSET;
+        int valueEndIndex = json.indexOf("]", keyEndIndex);
+
+        if (valueEndIndex - keyEndIndex <= 1) {
+            return null;
+        }
+
+        String commaSeparatedValues = json.substring(keyEndIndex, valueEndIndex);
+        String[] values = commaSeparatedValues.split(",");
+
+        return new ArrayList<>(Arrays.asList(values));
+    }
+
+
 }
